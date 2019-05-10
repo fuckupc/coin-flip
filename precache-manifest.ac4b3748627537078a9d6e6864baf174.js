a13d84989652c48f6edae6ce7b4182e375a891a8
upcliucong <upcliucong@gmail.com>
2019-05-10 12:19:46 +0800

    Updates

---
+++ precache-manifest.ac4b3748627537078a9d6e6864baf174.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8267c67445c6cee5cd1b518cf4d24a",
+    "url": "/coin-flip/index.html"
+  },
+  {
+    "revision": "53531e279f77c4dfc94c",
+    "url": "/coin-flip/static/css/main.a47e8e35.chunk.css"
+  },
+  {
+    "revision": "9c9c31d921725c64fea1",
+    "url": "/coin-flip/static/js/2.b4f176e3.chunk.js"
+  },
+  {
+    "revision": "53531e279f77c4dfc94c",
+    "url": "/coin-flip/static/js/main.7cc95b5e.chunk.js"
+  },
+  {
+    "revision": "8798ee5a17be05605147",
+    "url": "/coin-flip/static/js/runtime~main.83c73e8c.js"
+  },
+  {
+    "revision": "43f803d7f5b9706cade56fa3733a8e39",
+    "url": "/coin-flip/static/media/bottom.43f803d7.jpg"
+  },
+  {
+    "revision": "99a107299db97ffe8a1491d5427ba596",
+    "url": "/coin-flip/static/media/top.99a10729.jpg"
+  }
+]);
